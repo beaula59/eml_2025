4cffc009a3bca217b415ac2ce83950fcb5670cf8
Bhavitha <159715811+Bhavitha88@users.noreply.github.com>
2025-02-14 12:44:24 +0530

    Add files via upload

--- src/app/team/multimedia/page.tsx
+++ src/app/team/multimedia/page.tsx
@@ -1,1 +1,23 @@
+import Animation from "../components/animation";
+import Menu from "../components/menu";
+import Head from "../components/head";
 
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen">
+      <Animation />
+      <Menu/>
+
+      <Head 
+        imageSrc="/images/design-head.jpg" 
+        title="Design and PR Head" 
+        name=" . " 
+        rollNo="co22btech11008" 
+        bgColor="bg-[#B0C4DE]" 
+        positionClass="absolute top-[220px] left-[60%] -translate-x-1/2"
+        textColor="text-[#1E456E]" 
+
+        />
+      </div>
+  );
+}
