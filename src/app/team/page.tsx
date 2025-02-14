3e38c20d40bc243072454ebaa35bfe2d464261df
Bhavitha <159715811+Bhavitha88@users.noreply.github.com>
2025-02-14 12:39:01 +0530

    Add files via upload

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,1 +1,15 @@
+import Animation from "./components/animation";
+import Menu from "./components/menu";
+import Overall from "./components/Overall";
+import Teambutton from "./components/team-button";
 
+export default function Home() {
+  return (
+    <div className="bg-white min-h-screen">
+      <Animation />
+      <Menu/>
+      <Overall/>
+      <Teambutton/>
+    </div>
+  );
+}
