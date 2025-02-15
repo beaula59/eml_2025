a4dce3be707d8a84817233c3d1c593a388d94a4d
Bhavitha <159715811+Bhavitha88@users.noreply.github.com>
2025-02-15 14:05:15 +0530

    Update page.tsx

--- src/app/team/multimedia/page.tsx
+++ src/app/team/multimedia/page.tsx
@@ -4,15 +4,15 @@ import Head from "../components/head";
 
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen">
+    <div className="bg-white min-h-screen relative">
       <Animation />
       <Menu/>
 
       <Head 
-        imageSrc="/images/design-head.jpg" 
-        title="Design and PR Head" 
-        name=" . " 
-        rollNo="co22btech11008" 
+        imageSrc="/images/Rajnil Malviya.jpeg" 
+        title="Multimedia Coordinator" 
+        name="Rajnil Malviya" 
+        rollNo="ee23btech11051" 
         bgColor="bg-[#B0C4DE]" 
         positionClass="absolute top-[220px] left-[60%] -translate-x-1/2"
         textColor="text-[#1E456E]" 
@@ -21,3 +21,4 @@ export default function Home() {
       </div>
   );
 }
+
