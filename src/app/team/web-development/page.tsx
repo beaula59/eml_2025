eb113e6c9e557626a5d5c06b6044d644d44c140a
Bhavitha <159715811+Bhavitha88@users.noreply.github.com>
2025-02-15 14:07:02 +0530

    Update page.tsx

--- src/app/team/web-development/page.tsx
+++ src/app/team/web-development/page.tsx
@@ -9,18 +9,18 @@ export default function DesignPR() {
         <Animation />
         <Menu/>
         <Head 
-        imageSrc="/images/design-head.jpg" 
-        title="Design and PR Head" 
-        name=" . " 
-        rollNo="co22btech11008"
+        imageSrc="/images/Beaula Mahima V.jpeg" 
+        title="Web Development" 
+        name="Beaula Mahima V" 
+        rollNo="ma21btech11002"
         bgColor="bg-[#1E456E]" 
         positionClass="absolute top-[120px] left-[60%] -translate-x-1/2"
       />
        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 absolute top-[270px] left-[400px] mt-10 w-[800px] h-[300px] p-5">
-          <Coordinator imageSrc="/images/design-head.jpg" title="Coordinator 1" name="." rollNo="." />
-          <Coordinator imageSrc="/images/design-head.jpg" title="Coordinator 2" name="." rollNo="." />
-          <Coordinator imageSrc="/images/design-head.jpg" title="Coordinator 3" name="." rollNo="." />
-          <Coordinator imageSrc="/images/design-head.jpg" title="Coordinator 4" name="." rollNo="." />
+          <Coordinator imageSrc="/images/Krishna Kumar.jpg" title="Coordinator" name="Krishna Kumar" rollNo="em23mtech11006" />
+          <Coordinator imageSrc="/images/Namagiri Tejasri.jpg" title="Coordinator" name="Namagiri Tejasri" rollNo="ep23btech11018" />
+          <Coordinator imageSrc="/images/Bhavitha.jpeg" title="Coordinator" name="Kakkirala Bhavitha" rollNo="co22btech11005" />
+          <Coordinator imageSrc="/images/Vasana Manas.jpg" title="Coordinator" name="Vasana Manas" rollNo="ee23btech11062" />
         </div>
 
 
