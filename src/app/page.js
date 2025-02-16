3ed173b48c3c82228786cac9e7abb4a1e353e3a5
Krishna Kumar <krishnakjee2016@gmail.com>
2025-02-17 04:15:45 +0530

    Conflict fix.

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import TextSection from "./components/TextSection";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import TextSection from "../components/TextSection";
 import Carousel from "../components/Carousel";
 
 export default function Home() {
