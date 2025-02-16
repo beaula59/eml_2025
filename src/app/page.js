ff8eefb5d598a94236baeed95f3c54dc8b5626c4
Krishna Kumar <krishnakjee2016@gmail.com>
2025-02-16 20:04:09 +0530

    brnahc

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,16 @@
 import Image from "next/image";
-<<<<<<< HEAD
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TextSection from "./components/TextSection";
-import Carousel from "./components/Carousel";
-=======
-import Navbar from "../components/Navbar";
->>>>>>> c10b426f32d054628ef9941c8a285d9a6b89103e
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
     <Navbar />
     <Hero/>
     <TextSection/>
     <Carousel/>
-=======
-      <Navbar />
-      <div className="font-bold text-[30px]">Home page</div>
->>>>>>> c10b426f32d054628ef9941c8a285d9a6b89103e
     </>
   );
 }
