0c673eb8e54796963344da4fadca823d6b87d2d9
Bhavitha <159715811+Bhavitha88@users.noreply.github.com>
2025-02-18 22:25:49 +0530

    Update page.tsx

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,14 +2,26 @@ import Animation from "../../components/animation";
 import Menu from "../../components/menu";
 import Overall from "../../components/Overall";
 import Teambutton from "../../components/team-button";
+import Navbar from "../../components/Navbar";
 
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen">
-      <Animation />
-      <Menu />
-      <Overall />
+    <div className="bg-white h-screen relative flex flex-col items-center overflow-hidden">
+      {/* Keep Animation only on larger screens */}
+      <div className="hidden sm:block">
+        <Animation />
+      </div>
+      <div className="flex flex-col"><Navbar/>
+      <div className="flex flex-row">
+      <Menu/>
+      <div className="flex flex-col">
       <Teambutton />
+      <Overall />
+      </div>
+      </div>
+      </div>
+      
     </div>
   );
 }
+
